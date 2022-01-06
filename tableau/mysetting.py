daf810337473abcc9276978c71d04d360e8f13fe
longfengpili <398745129@qq.com>
2022-01-06 08:42:23 +0800

    [新增]mysetting

---
+++ tableau/mysetting.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Author: chunyang.xu
+# @Date:   2022-01-04 20:47:41
+# @Last Modified by:   chunyang.xu
+# @Last Modified time: 2022-01-06 08:41:47
+
+APP_ID = 'appkMAWugcQ3660'
+DOMAIN = 'appkmawugcq3660.pc.xiaoe-tech.com'
+COURSESAPI = f'https://{DOMAIN}/api/xe.goods.relation.get/1.0.0?app_id={APP_ID}'
+COURSEAPI = f'https://{DOMAIN}/api/xe.goods.detail.get/2.0.0?app_id={APP_ID}'
+
+HEADERS = {
+    'app-id': 'appkMAWugcQ3660',
+    'content-length': '187',
+    'content-type': 'application/json;charset=UTF-8',
+    'host': 'appkmawugcq3660.pc.xiaoe-tech.com',
+    'origin': 'https://appkmawugcq3660.pc.xiaoe-tech.com',
+    'referer': 'https://appkmawugcq3660.pc.xiaoe-tech.com/detail/term_61c55f79648f9_RYHNiw/25',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
+}
+
