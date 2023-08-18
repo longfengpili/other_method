b8c0aaa33a0a983681412fc03ed8ef9b901202ed
longfengpili <398745129@qq.com>
2023-08-18 12:10:09 +0800

    [删除]headers

--- device_info/devices/device/geekbench.py
+++ device_info/devices/device/geekbench.py
@@ -2,7 +2,7 @@
 # @Author: longfengpili
 # @Date:   2023-08-11 15:27:12
 # @Last Modified by:   longfengpili
-# @Last Modified time: 2023-08-17 17:24:18
+# @Last Modified time: 2023-08-18 12:08:51
 # @github: https://github.com/longfengpili
 
 from lxml.etree import Element as elem
@@ -33,14 +33,13 @@ class Geekbench(PhoneBase):
         base_url = 'https://browser.geekbench.com'
         return base_url
 
-    @property
-    def headers(self):
-        headers = {
-            'Cookie': '_browse_session=B7U7kJoPc9ZGhUOnI87iXXoBGVOSFB7cdLqCePaN9nRrbrKurIKjJNM000s%2B3Hn%2F1X%2Fzuxeq%2Bnzheuiij0Ocb9E6rV2p7M5NWV51bnSq%2F1cq5cSrrQv0cU0ea6KtfxhGucqmplFHLWFWevZgqqhaJAWJ%2BasGb5QvSz%2Fm--rtR%2FHvz22%2BtTGsrA--SBxnSdo6e8EsST1U8Z0Aaw%3D%3D',  # noqa: E501
-            'Host': 'browser.geekbench.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-        }
-        return headers
+    # @property
+    # def headers(self):
+    #     headers = {
+    #         'Host': 'browser.geekbench.com',
+    #         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    #     }
+    #     return headers
 
     def request(self, pname: str):
         url = f"{self.base_url}/search"
