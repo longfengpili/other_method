db97bcd3cf1f6a8eec78035934ca2e56fec5194a
longfengpili <398745129@qq.com>
2024-09-01 11:09:35 +0800

    [优化]ffmpeg

--- myffmpeg.py
+++ myffmpeg.py
@@ -2,12 +2,17 @@
 # @Author: longfengpili
 # @Date:   2024-08-23 10:51:34
 # @Last Modified by:   longfengpili
-# @Last Modified time: 2024-08-23 10:51:52
+# @Last Modified time: 2024-09-01 11:08:45
 # @github: https://github.com/longfengpili
 
 from pathlib import Path
 
+from pydbapi.conf import LOGGING_CONFIG
 import ffmpeg
+
+import logging
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
 
 
 def get_files(path: str = None):
@@ -22,14 +27,17 @@ def convert_video(file: Path, target_path: str = None, r: int = 60):
     target_path = target_path or f'{file.drive}/'
     tpath = Path(target_path) / 'new_video'
     tfile = tpath / f'new_{file.name}'
+    tfile = tfile.as_posix()
 
     if not tpath.exists():
         tpath.mkdir(parents=True)
 
-    ffmpeg.input(file).output(tfile, r=r)
+    ffmpeg.input(file).output(tfile, r=r).run()
 
 
 if __name__ == '__main__':
     files = get_files()
     for file in files:
+        logger.info(file)
         convert_video(file)
+        break
